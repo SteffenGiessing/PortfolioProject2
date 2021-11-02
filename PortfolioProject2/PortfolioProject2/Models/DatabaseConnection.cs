8323224e8fb528566b38ce4855176c6ab00a09df
Steffen Giessing <Steffen.S.Giessing@gmail.com>
2021-11-02 13:46:17 +0100

    remote database setup

--- PortfolioProject2/PortfolioProject2/Models/DatabaseConnection.cs
+++ PortfolioProject2/PortfolioProject2/Models/DatabaseConnection.cs
@@ -5,6 +5,7 @@ namespace PortfolioProject2.Models
 {
     public class DatabaseConnection : DbContext
     {
+        private readonly string connectionString = "host=rawdata.ruc.dk;port=5432;database=raw8;username=raw8;password=utuBOPUw";
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -13,7 +14,7 @@ namespace PortfolioProject2.Models
             optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
             optionsBuilder.EnableSensitiveDataLogging();
 
-            optionsBuilder.UseNpgsql("host=localhost;db=imdb_new;uid=postgres;pwd=");
+            optionsBuilder.UseNpgsql(connectionString);
             
             
             Console.WriteLine("Hallo");
