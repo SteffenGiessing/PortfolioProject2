6b072aa440493d2750917b3d0c3c7e10b4528a8a
Steffen Giessing <Steffen.S.Giessing@gmail.com>
2021-11-02 13:23:51 +0100

    init

---
+++ PortfolioProject2/PortfolioProject2/Models/DatabaseConnection.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using System;
+namespace PortfolioProject2.Models
+{
+    public class DatabaseConnection : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
+            optionsBuilder.EnableSensitiveDataLogging();
+
+            optionsBuilder.UseNpgsql("host=localhost;db=imdb_new;uid=postgres;pwd=Gie2761!");
+        }
+    }
+}
---
+++ PortfolioProject2/PortfolioProject2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PortfolioProject2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
