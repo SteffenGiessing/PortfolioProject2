59e47cc50d63b07fe30f6438e80863f886b3564f
Steffen Giessing <Steffen.S.Giessing@gmail.com>
2021-11-02 14:06:14 +0100

    database connection for remote database and title tabl

---
+++ PortfolioProject2/PortfolioProject2/Models/DMOs/Titles.cs
@@ -0,0 +1,16 @@
+namespace PortfolioProject2.Models.DMOs
+{
+    public class Titles
+    {
+        public string titlesid { get; set; }
+        public string titletype { get; set; }
+        public string primarytitle { get; set; }
+        public string originaltitle { get; set; }
+        public bool isadult { get; set; }
+        public string startyear { get; set; }
+        public string endyear { get; set; }
+        public int runtime { get; set; }
+        public string genres { get; set; }
+        
+    }
+}
--- PortfolioProject2/PortfolioProject2/Models/DatabaseConnection.cs
+++ PortfolioProject2/PortfolioProject2/Models/DatabaseConnection.cs
@@ -1,10 +1,14 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using System;
+using PortfolioProject2.Models.DMOs;
+
 namespace PortfolioProject2.Models
 {
     public class DatabaseConnection : DbContext
     {
+        public DbSet<Titles> titles { get; set; }
+        
         private readonly string connectionString = "host=rawdata.ruc.dk;port=5432;database=raw8;username=raw8;password=utuBOPUw";
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -16,8 +20,23 @@ namespace PortfolioProject2.Models
 
             optionsBuilder.UseNpgsql(connectionString);
             
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
             
-            Console.WriteLine("Hallo");
+            modelBuilder.Entity<Titles>().ToTable("titles");
+            modelBuilder.Entity<Titles>().Property(x => x.titlesid).HasColumnName("titleid");
+            modelBuilder.Entity<Titles>().Property(x => x.titletype).HasColumnName("titletype");
+            modelBuilder.Entity<Titles>().Property(x => x.primarytitle).HasColumnName("primarytitle");
+            modelBuilder.Entity<Titles>().Property(x => x.originaltitle).HasColumnName("originaltitle");
+            modelBuilder.Entity<Titles>().Property(x => x.isadult).HasColumnName("isadult");
+            modelBuilder.Entity<Titles>().Property(x => x.startyear).HasColumnName("startyear");
+            modelBuilder.Entity<Titles>().Property(x => x.endyear).HasColumnName("endyear");
+            modelBuilder.Entity<Titles>().Property(x => x.runtime).HasColumnName("runtime");
+            modelBuilder.Entity<Titles>().Property(x => x.genres).HasColumnName("genres");
+
         }
     }
 }
