e12826e2cb9c78c0306db6961e984a4a9c48981f
Jonas <jdeichmann@protonmail.com>
2021-11-04 22:40:22 +0100

    Added Actor Dataservice, IDataservice and controller

---
+++ PortfolioProject2/WebApplication/Controllers/ActorController.cs
@@ -0,0 +1,28 @@
+﻿#nullable enable
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PortfolioProject2.Models.DataInterfaces;
+using PortfolioProject2.Models.DMOs;
+
+namespace WebApplication.Controllers
+{
+    [ApiController]
+    [Route("api/actor")]
+    public class ActorController : Controller
+    {
+        private readonly IActorDataService _iDataServices;
+
+        public ActorController(IActorDataService dataServices)
+        {
+            _iDataServices = dataServices;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Person_Info>> GetActors()
+        {
+            var actor = _iDataServices.GetAllActors();
+            return Ok(actor);
+        }
+    }
+}
