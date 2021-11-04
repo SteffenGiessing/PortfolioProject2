ae7a226de343f93d75d34f51cc21519e70b1e11a
Jonas <jdeichmann@protonmail.com>
2021-11-04 09:38:40 +0100

    Added all primary keys to DataBaseConnections and minor adjustment to User_Bookmarks.cs

--- PortfolioProject2/PortfolioProject2/Models/DMOs/User_BookMarks.cs
+++ PortfolioProject2/PortfolioProject2/Models/DMOs/User_BookMarks.cs
@@ -2,6 +2,7 @@ namespace PortfolioProject2.Models.DMOs
 {
     public class User_BookMarks
     {
+        public string BookMarkId { get; set; }
         public string UserId { get; set; }
         
         public string TitleId { get; set; }
