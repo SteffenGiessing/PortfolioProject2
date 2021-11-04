4164b06f50ff40a1f02c4ac629b048fcdd1c5005
Jonas <jdeichmann@protonmail.com>
2021-11-04 22:40:22 +0100

    Added Actor Dataservice, IDataservice and controller

---
+++ PortfolioProject2/PortfolioProject2/Models/DataInterfaces/IActorDataService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PortfolioProject2.Models.DMOs;
+
+//why wont this crap work
+
+namespace PortfolioProject2.Models.DataInterfaces
+{
+    public interface IActorDataService
+    {
+        IList<Person_Info> GetAllActors();
+
+    }
+}
---
+++ PortfolioProject2/PortfolioProject2/Models/DataServices/ActorDataService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PortfolioProject2.Models.DataInterfaces;
+using PortfolioProject2.Models.DMOs;
+
+
+namespace PortfolioProject2.Models.DataServices
+{
+    public class ActorDataService : IActorDataService
+    {
+        public IList<Person_Info> GetAllActors()
+        {
+            using var ctx = new DatabaseConnection();
+            IList<Person_Info> list = ctx.Person_Info.ToList();
+            return list;
+        }
+    }
+}
