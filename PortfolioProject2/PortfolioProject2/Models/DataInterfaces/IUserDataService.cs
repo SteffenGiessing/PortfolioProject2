80eb76b6feb524c4892e40968d55c1b20045f16a
Burnirr <andrei.sv97@yahoo.com>
2021-11-09 23:27:42 +0100

    Fixed the User Controllers

--- PortfolioProject2/PortfolioProject2/Models/DataInterfaces/IUserDataService.cs
+++ PortfolioProject2/PortfolioProject2/Models/DataInterfaces/IUserDataService.cs
@@ -7,7 +7,7 @@ namespace PortfolioProject2.Models.DataInterfaces
     public interface IUserDataService
     {
         IList<User_Comments> GetAllComments();
-        
+
         Task<List<User_Comments>> GetAllCommentsFromOneUser(string userid);
 
         Task<List<User_Comments>> GetAllCommentsFromOneTitle(string titleid);
