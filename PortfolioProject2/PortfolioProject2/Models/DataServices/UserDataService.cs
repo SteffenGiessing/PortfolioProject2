31b8ed7041ee37bd9ae53fcf7d2364d82ea36f03
Sger <awesomethymes@gmail.com>
2021-11-09 22:41:07 +0100

    added User (interface, dataservice and controller)

---
+++ PortfolioProject2/PortfolioProject2/Models/DataServices/UserDataService.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PortfolioProject2.Models.DataInterfaces;
+using PortfolioProject2.Models.DMOs;
+
+namespace PortfolioProject2.Models.DataServices
+{
+    public class UserDataService : IUserDataService
+    {
+        public IList<User_Comments> GetAllComments()
+        {
+            using var ctx = new DatabaseConnection();
+            IList<User_Comments> list = ctx.User_Comments.ToList();
+            return list;
+        }
+        
+        public async Task<List<User_Comments>> GetAllCommentsFromOneUser(string userid)
+        {
+            var ctx = new DatabaseConnection();
+            return await ctx.User_Comments
+                .FromSqlRaw("SELECT * FROM user_comments WHERE userid = {0}", userid)
+                .ToListAsync();
+        }
+        
+        public async Task<List<User_Comments>> GetAllCommentsFromOneTitle(string titleid)
+        {
+            var ctx = new DatabaseConnection();
+            return await ctx.User_Comments
+                .FromSqlRaw("SELECT * FROM user_comments WHERE titleid = {0}", titleid)
+                .ToListAsync();
+        }
+        
+    }
+}
+
---
+++ PortfolioProject2/WebApplication/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioProject2.Models.DataInterfaces;
+#nullable enable
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using PortfolioProject2.Models.DMOs;
+
+namespace WebApplication.Controllers
+{
+    [ApiController]
+    [Route("api/user")]
+    
+        public class UserController : Controller
+        {
+            private readonly IUserDataService _iDataServices;
+
+            public UserController(IUserDataService dataServices)
+            {
+                _iDataServices = dataServices;
+            }
+            
+            [HttpGet]
+            public ActionResult<IEnumerable<User_Comments>> GetAllComments()
+            {
+                var comments = _iDataServices.GetAllComments();
+                return Ok(comments);
+            }
+            
+            [HttpGet("{userid}")]
+            public async Task<ActionResult<User_Comments>> GetAllCommentsFromOneUser(string? userid)
+            {
+                var allCommentsFromOneUser = _iDataServices.GetAllCommentsFromOneUser(userid).Result;
+                return Ok(allCommentsFromOneUser);
+            }
+            
+            [HttpGet("{titleid}")]
+            public async Task<ActionResult<User_Comments>> GetAllCommentsFromOneTitle(string? titleid)
+            {
+                var allCommentsFromOneTitle = _iDataServices.GetAllCommentsFromOneTitle(titleid).Result;
+                return Ok(allCommentsFromOneTitle);
+            }
+    }
+}
+
+
