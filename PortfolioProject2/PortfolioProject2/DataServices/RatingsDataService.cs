9d2ca27232afed16c03ee9c56503df5116fd1d4a
Sger <awesomethymes@gmail.com>
2021-11-11 22:04:50 +0100

    naming

--- PortfolioProject2/PortfolioProject2/DataServices/RatingsDataService.cs
+++ PortfolioProject2/PortfolioProject2/DataServices/RatingsDataService.cs
@@ -8,7 +8,7 @@ namespace PortfolioProject2.Models.DataServices
 {
     public class RatingsDataService : IRatings
     {
-        public async Task<List<Ratings>> getRaitingById(string id)
+        public async Task<List<Ratings>> GetRatingBy(string id)
         {
             var ctx = new DatabaseConnection();
             return await ctx.Ratings.FromSqlRaw("SELECT * FROM ratings WHERE titleid = {0}", id).ToListAsync();
