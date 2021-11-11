9b30f6b44689896b6cc879f96639d740b59f43a4
Jonas <jdeichmann@protonmail.com>
2021-11-11 13:28:20 +0100

    added title_search

---
+++ PortfolioProject2/PortfolioProject2/Models/DMOs/Title_Search.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PortfolioProject2.Models.DMOs
+{
+    public class TitleSearch
+    {
+        public string PrimaryTitle { get; set; }
+        
+        public string TitleId { get; set; }
+    }
+}
