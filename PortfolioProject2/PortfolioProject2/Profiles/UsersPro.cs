5e7c89820bcafa4cca6d0b8528036dac619f201e
Steffen Giessing <Steffen.S.Giessing@gmail.com>
2021-11-16 11:46:45 +0100

    just some useless user stuff i dont want to get lost

---
+++ PortfolioProject2/PortfolioProject2/Profiles/UsersPro.cs
@@ -0,0 +1,16 @@
+/*
+using AutoMapper;
+using PortfolioProject2.Models.DMOs;
+
+namespace PortfolioProject2.Profiles
+{
+    public class UsersPro : Profile
+    {
+        public UsersPro()
+        {
+            CreateMap<Users, User_User>();
+            CreateMap<UserToCreate, Users>();
+        }
+    }
+}
+*/
